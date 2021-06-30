2592c1a2d14f4848dceb01ae0b6bb51953c30420
Karl <enriquezkharl14@gmail.com>
2021-06-30 11:10:37 +0800

    Insertion Sort

---
+++ insertionSort.js
@@ -0,0 +1,3 @@
+//Start by picking the second element of the array
+//Now compare the second element with the one before it and swap if necessary
+//Continue to the next element and if it is iin the incorrect order, iterate through the sorted portion
