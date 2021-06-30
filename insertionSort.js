781fe7506b9c58bf7f550a5e456083fb1fc9891f
karl <enriquezkharl14@gmail.com>
2021-06-30 12:10:01 +0800

    Insertion Sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,19 @@
 //Start by picking the second element of the array
 //Now compare the second element with the one before it and swap if necessary
 //Continue to the next element and if it is iin the incorrect order, iterate through the sorted portion
+//Repeat until array is sorted
+
+function insertionSort(array) {
+  let j;
+  for (let i = 1; i < array.length; i++) {
+    let currentValue = array[i];
+    for (j = i - 1; j >= 0 && array[j] > currentValue; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = currentValue;
+  }
+
+  return array;
+}
+
+//Time complexity On2 worst case
